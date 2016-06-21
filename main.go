0db159ea8edc684d618efbe16423762dfffa0106
Tim Donlan <tim.donlan@patientco.com>
2016-06-21 22:17:38 +0100

    Added gin router

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import ("fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"KeyValTest/model"
+	"KeyValTest/router"
 	"encoding/json"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -13,24 +15,45 @@ func main() {
 	model.ResetDB(dbName)
 	model.InitDB(dbName)
 	model.CreateKeyVal("hello", "world")
+	model.CreateKeyVal("world2", "hello2")
+	router.StartService()
+
+}
+
+func testDB() {
+	dbName := "KeyVal.db"
+
+	model.ResetDB(dbName)
+	model.InitDB(dbName)
+	model.CreateKeyVal("hello", "world")
 
 	model.CreateKeyVal("world2", "hello2")
 
-	model.UpdateKeyVal("hello","world3")
+	model.UpdateKeyVal("hello", "world3")
 	model.DeleteKeyVal("world2")
 
-	dataArray,err := model.GetAllKeyVal()
-	if(err != nil){
+	dataArray, err := model.GetAllKeyVal()
+	if (err != nil) {
 		return
 	}
 
-	json,err := json.Marshal(dataArray)
+	json, err := json.Marshal(dataArray)
 	fmt.Printf("%s", json)
 
-	for _, data := range dataArray{
-		if(data != nil) {
+	for _, data := range dataArray {
+		if (data != nil) {
 
-		fmt.Printf("%s: %s\n",data.Key, data.Value)
+			fmt.Printf("%s: %s\n", data.Key, data.Value)
 		}
 	}
+}
+
+func testGin() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and server on 0.0.0.0:8080
 }
---
+++ router/KeyValService.go
@@ -0,0 +1,70 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"KeyValTest/model"
+)
+
+func StartService(){
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/key/:key",getKeyVal)
+	r.GET("/keys",getAllKeyVals)
+	r.POST("/key", createKeyVal)
+
+
+	r.Run() // listen and server on 0.0.0.0:8080
+}
+
+func getKeyVal(c *gin.Context) {
+	key := c.Param("key")
+
+	//c.JSON(200, key)
+	//return
+
+	keyValData, err := model.GetKeyVal(key)
+	if (err != nil) {
+		c.JSON(500, gin.H{"error":err})
+	}else if keyValData != nil {
+		c.JSON(200, keyValData)
+	} else {
+		c.JSON(404, gin.H{"error": "Not Found"})
+	}
+
+}
+
+func getAllKeyVals(c *gin.Context){
+	keyValArray, err := model.GetAllKeyVal()
+	if (err != nil) {
+		c.JSON(500, gin.H{"error":err})
+	} else if keyValArray != nil {
+		c.JSON(200, keyValArray)
+	} else {
+		c.JSON(404, gin.H{"error": "Not Found"})
+	}
+
+}
+
+func createKeyVal(c *gin.Context){
+	var newKeyVal model.KeyValData
+
+	if c.Bind(&newKeyVal) == nil {
+		keyValData, err := model.CreateKeyVal(newKeyVal.Key, newKeyVal.Value)
+		if (err != nil) {
+			c.JSON(500, gin.H{"error":err})
+		} else if keyValData != nil {
+			c.JSON(200, keyValData)
+		} else {
+			c.JSON(500, gin.H{"error": "Unable to create"})
+		}
+	}
+}
+
+func getAllKeyVal(c *gin.Context){
+
+}
