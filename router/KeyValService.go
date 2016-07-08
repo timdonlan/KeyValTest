b3a7a8aa85a53bf485c9064f78ad26e27685c9b9
Tim Donlan <tim.donlan@patientco.com>
2016-07-08 11:10:41 -0400

    Changed default port

--- router/KeyValService.go
+++ router/KeyValService.go
@@ -18,7 +18,7 @@ func StartService(){
 	r.POST("/key", createKeyVal)
 
 
-	r.Run() // listen and server on 0.0.0.0:8080
+	r.Run(":8081") // listen and server on 0.0.0.0:8081
 }
 
 func getKeyVal(c *gin.Context) {
